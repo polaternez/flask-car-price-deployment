078fc0a73312e1bb5a1fd40dc26fd25eec44975d
Polat Ernez <93451337+polaternez@users.noreply.github.com>
2021-12-02 12:48:24 +0300

    Update app.py

--- car-price-deployment-flask/app.py
+++ car-price-deployment-flask/app.py
@@ -19,16 +19,7 @@ def predict():
 
     return render_template("index.html", prediction_text="Car price should be $ {}".format(output))
         
-@app.route("/predict_api", methods=["POST"])
-def predict_api():
-    '''
-    For direct API calls trought request
-    '''
-    data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
 
-    output = prediction[0]
-    return jsonify(output)
 
 if __name__ == "__main__":
     app.run(debug=True)
